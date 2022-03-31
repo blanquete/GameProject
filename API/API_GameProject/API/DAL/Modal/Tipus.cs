abd3a82b629ca28dc63125673284868b8f0546e4
iprats <iprats.dam@institutcampalans.net>
2022-03-31 16:30:49 +0200

    Creacio i Inici de l'API
    He creat el projecte d'Aplicacio Web que sera l'API del nostre projecte. He començat a crear una mica l'estructura de les carpetes i he entrat ja 3 Models dels objectes que tenim (Personatge, Tipus, Classe) - Creacio i Inici de l'API

---
+++ API/API_GameProject/API/DAL/Modal/Tipus.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.DAL.Model
+{
+    class Tipus
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("source")]
+        public string Source { get; set; }
+
+        [BsonElement("condition")]
+        public string Condition { get; set; }
+
+        [BsonElement("chatFlavor")]
+        public string ChatFlavor { get; set; }
+
+        [BsonElement("requirements")]
+        public string Requirements { get; set; }
+    }
+}
