2a66df7493991b72a99cae07e7ce62a3306e363b
iprats <iprats.dam@institutcampalans.net>
2022-05-04 15:24:35 +0200

    .

---
+++ API/API_GameProject/API/DAL/Model/Scene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace API.DAL.Model
+{
+    public class Scene
+    {
+        public ObjectId O_Id { get; set; }
+        public int Id { get; set; }
+        public int partida_id { get; set; }
+        public byte[] image { get; set; }
+        public int num { get; set; }
+        public Monster [] monsters { get; set; }
+    }
+}
